b0b603dbab163ece5bcb24a53df6293a11e1df51
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-09-11 10:59:35 +0530

    Commit : Protecting resource integrity using transaction

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -9,4 +9,11 @@ router.post('/bookings', bookingController.createBooking);
 router.post('/publish', bookingController.sendMessageToQueue);
 router.patch('/bookings/:id', bookingController.cancelBooking);
 
+router.get('/hello', (req, res) => { // localhost:3000/api/v1/hello
+    return res.status(200).json({
+        message: 'Hello User',
+        status: true
+    })
+})
+
 module.exports = router;
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const {FLIGHT_SERVICE_PATH, AUTH_SERVICE_PATH, REMAINDER_BINDING_KEY} = require('../config/serverConfig')
 const { BookingRepository } = require('../repository/index');
 const { ServiceError } = require('../utils/errors');
+const db = require('../models/index')
 
 const { createChannel, publishMessage } = require('../utils/messageQueue');
 
@@ -40,37 +41,43 @@ class BookingService {
 
     async createBooking (data){
         try {
-            const flightId = data.flightId;
-            const getFlightRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
-            const flightResponse = await axios.get(getFlightRequestUrl);
-            // console.log(flightResponse)
-            const flightData = flightResponse.data.data;
+            const response = await db.sequelize.transaction(async (t) => {
 
-            let priceOfTheFlight = flightData.price;
-            if(data.noOfSeats > flightData.totalSeats){
-                throw new ServiceError (
-                    'ServiceError',
-                    'Something went wrong in the booking process',
-                    'Insufficient seats in the flight',
-                )
-            }
-            const totalCosts = priceOfTheFlight * data.noOfSeats;
-            const bookingPayload = {...data, totalCosts};
-            const booking = await this.bookingRepository.create(bookingPayload);
-            console.log(booking)
-            const updateFlightRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`
-            await axios.patch(updateFlightRequestUrl, {totalSeats: flightData.totalSeats - booking.noOfSeats})
-            const finalBooking = await this.bookingRepository.update(booking.id, {status: "Booked"});
-
-            const getUserRequestUrl = `${AUTH_SERVICE_PATH}/api/v1/users/${booking.userId}`;
-            const userResponse = await axios.get(getUserRequestUrl);
-            const userDetails = userResponse.data.data;
+                const flightId = data.flightId;
+                const getFlightRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
+                const flightResponse = await axios.get(getFlightRequestUrl);
             
-            console.log(userDetails);
-            const ticket = {...flightData, ...userDetails, ...finalBooking};
-            const channel = await createChannel();
-            await publishMessage(channel, REMAINDER_BINDING_KEY, JSON.stringify(ticket));
-            return ticket;
+                // console.log(flightResponse.data)
+                const flightData = flightResponse.data.data;
+    
+                let priceOfTheFlight = flightData.price;
+                if(data.noOfSeats > flightData.totalSeats){
+                    throw new ServiceError (
+                        'ServiceError',
+                        'Something went wrong in the booking process',
+                        'Insufficient seats in the flight',
+                    )
+                }
+                const totalCosts = priceOfTheFlight * data.noOfSeats;
+                const bookingPayload = {...data, totalCosts};
+                const booking = await this.bookingRepository.create(bookingPayload);
+                console.log(booking)
+                const updateFlightRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`
+                await axios.patch(updateFlightRequestUrl, {totalSeats: flightData.totalSeats - booking.noOfSeats})
+                const finalBooking = await this.bookingRepository.update(booking.id, {status: "Booked"});
+    
+                const getUserRequestUrl = `${AUTH_SERVICE_PATH}/api/v1/users/${booking.userId}`;
+                const userResponse = await axios.get(getUserRequestUrl);
+                const userDetails = userResponse.data.data;
+                
+                console.log(userDetails);
+                const ticket = {...flightData, ...userDetails, ...finalBooking};
+                const channel = await createChannel();
+                await publishMessage(channel, REMAINDER_BINDING_KEY, JSON.stringify(ticket));
+                return ticket;
+            })
+            // console.log(response);
+            return response
         } catch (error) {
             console.log(error)
             if(error.name == 'RepositoryError' || error.name == 'ValidationError') {
@@ -81,27 +88,31 @@ class BookingService {
     }
     async cancelFlight(bookingId){
         try {
-            const bookingDetails = await this.bookingRepository.get(bookingId);
-            const bookedTime = bookingDetails.createdAt;
-            
-            const flightDetailsRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${bookingDetails.flightId}`;
-            const flightDetailsResponse = await axios.get(flightDetailsRequestUrl);
-            const flightDetails = flightDetailsResponse.data.data;
-
-            const isCancellable = this.#getRefundableAmount(bookedTime, flightDetails.departureTime, flightDetails.price);
-            console.log(isCancellable)
-            if(isCancellable){
-                const cancelBooking = await this.bookingRepository.update(bookingId, {status: "Cancelled"});
-                const updateFlightDetailsRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${bookingDetails.flightId}`;
-                const finalResponse = await axios.patch(updateFlightDetailsRequestUrl, {totalSeats: flightDetails.totalSeats + bookingDetails.noOfSeats});
-                return {...cancelBooking.dataValues, refundableAmount: isCancellable.refundableAmount};
-            } else{
-                throw new ServiceError(
-                    'ServiceError',
-                    'An issue occurred during the cancellation process',
-                    "The flight cannot be cancelled at this stage. Please check the flight status or contact support for further assistance."
-                );
-            }
+            const response = await db.sequelize.transaction(async (t) => {
+                const bookingDetails = await this.bookingRepository.get(bookingId);
+                const bookedTime = bookingDetails.dataValues.createdAt;
+                // console.log(bookingDetails.dataValues.flightId);
+                const flightDetailsRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${bookingDetails.dataValues.flightId}`;
+                const flightDetailsResponse = await axios.get(flightDetailsRequestUrl);
+                const flightDetails = flightDetailsResponse.data.data;
+                // console.log(flightDetails);
+                const isCancellable = this.#getRefundableAmount(bookedTime, flightDetails.departureTime, flightDetails.price);
+                console.log(isCancellable)
+                if(isCancellable){
+                    const cancelBooking = await this.bookingRepository.update(bookingId, {status: "Cancelled"});
+                    const updateFlightDetailsRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${bookingDetails.dataValues.flightId}`;
+                    const finalResponse = await axios.patch(updateFlightDetailsRequestUrl, {totalSeats: flightDetails.totalSeats + bookingDetails.dataValues.noOfSeats});
+                    return {...cancelBooking.dataValues, refundableAmount: isCancellable.refundableAmount};
+                } 
+                else{
+                    throw new ServiceError(
+                        'ServiceError',
+                        'An issue occurred during the cancellation process',
+                        "The flight cannot be cancelled at this stage. Please check the flight status or contact support for further assistance."
+                    );
+                }
+            })
+            return response
         } catch (error) {
             console.log(error)
             if(error.name == 'RepositoryError' || error.name == 'ValidationError' || error.name == 'ServiceError') {
